880659d3de8822d3abde7a4f97800f46e3ca8053
Alina Osv <hixid@ya.ru>
2020-04-18 19:31:43 +0300

    Add other items test

--- index.test.js
+++ index.test.js
@@ -17,7 +17,7 @@ describe('Aged Brie should: ', () => {
         expect(items).toEqual(res);
     });
 
-    test('have quality +2 if sellIn passed', () => {
+    test('inc quality twice if sellIn passed', () => {
         const shop = new Shop([new Item('Aged Brie', 0, 0)]);
         const items = shop.updateQuality();
         const res = [{name: 'Aged Brie', sellIn: -1, quality: 2}];
@@ -74,11 +74,33 @@ describe('Sulfuras should: ', () => {
     });
 });
 
-/*describe('Other should: ', () => {
-    test('have quality > 0', () => {
-        const shop = new Shop([new Item('eggs', 1, 0)]);
+describe('Simple items should: ', () => {
+    const itemName = 'eggs';
+
+    test('decrease quality', () => {
+        const shop = new Shop([new Item(itemName, 3, 10)]);
+        const items = shop.updateQuality();
+        const res = [{name: itemName, sellIn: 2, quality: 9}];
+
+        expect(items).toEqual(res);
     });
-}); */
+
+    test('have quality >= 0', () => {
+        const shop = new Shop([new Item(itemName, 1, 0)]);
+        const items = shop.updateQuality();
+        const res = [{name: itemName, sellIn: 0, quality: 0}];
+
+        expect(items).toEqual(res);
+    });
+
+    test('decrease quality twice when sellIn < 0', () => {
+        const shop = new Shop([new Item(itemName, 0, 5)]);
+        const items = shop.updateQuality();
+        const res = [{name: itemName, sellIn: -1, quality: 3}];
+
+        expect(items).toEqual(res);
+    });
+});
 
 // Aged Brie
 // Backstage passes to a TAFKAL80ETC concert
