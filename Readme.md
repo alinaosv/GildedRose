220a4493c7bb29406d08c86f6e4a6da0eebaf63f
Alina Osv <hixid@ya.ru>
2020-04-18 19:08:14 +0300

    Add brie tests & improved Readme file

--- Readme.md
+++ Readme.md
@@ -6,11 +6,11 @@
 
 ##How things work:
 - Once the sell by date has passed, Quality degrades twice as fast
-- The Quality of an item is never negative
+- The Quality of an item is [0, 50];
 - “Aged Brie” actually increases in Quality the older it gets
-- The Quality of an item is never more than 50
 - “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
-- “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+- “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches; 
+Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
 
 ##ToDo:
 - “Conjured” items degrade in Quality twice as fast as normal items
--- index.test.js
+++ index.test.js
@@ -1,9 +1,83 @@
-const {Shop, Item} = require("./index");
+const {Shop, Item} = require('./index');
 
-describe("Gilded Rose", function() {
-    it("should foo", function() {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toBe("foo");
+describe('Aged Brie should: ', () => {
+    test('increase quality', () => {
+        const shop = new Shop([new Item('Aged Brie', 3, 25)]);
+        const items = shop.updateQuality();
+        const res = [{name: 'Aged Brie', sellIn: 2, quality: 26}];
+
+        expect(items).toEqual(res);
+    });
+
+    test('have quality <= 50', () => {
+        const shop = new Shop([new Item('Aged Brie', 3, 50)]);
+        const items = shop.updateQuality();
+        const res = [{name: 'Aged Brie', sellIn: 2, quality: 50}];
+
+        expect(items).toEqual(res);
+    });
+
+    test('have quality +2 if sellIn passed', () => {
+        const shop = new Shop([new Item('Aged Brie', 0, 0)]);
+        const items = shop.updateQuality();
+        const res = [{name: 'Aged Brie', sellIn: -1, quality: 2}];
+
+        expect(items).toEqual(res);
     });
 });
+
+/* describe('TAFKAL80ETC should: ', () => {
+    const itemName = 'Backstage passes to a TAFKAL80ETC concert';
+
+    test('Aged', () => {
+        const shop = new Shop([new Item(itemName, 0, 0)]);
+        const items = shop.updateQuality();
+        const res = [{name: itemName, sellIn: 2, quality: 50}];
+
+        expect(items).toEqual(res);
+    });
+});
+
+describe('Sulfuras should: ', () => {
+    let shop;
+    beforeEach(() => {
+        shop = new Shop([new Item('Sulfuras, Hand of Ragnaros', 0, 0)]);
+    });
+
+    test('Aged', () => {
+    });
+});
+
+describe('Other should: ', () => {
+    test('have quality > 0', () => {
+        const shop = new Shop([new Item('eggs', 1, 0)]);
+    });
+}); */
+
+// Aged Brie
+// Backstage passes to a TAFKAL80ETC concert
+// Sulfuras, Hand of Ragnaros
+
+/* 1. !AgedBrie && !TAFKAL80ETC
+    quality > 0 && !Sulfuras
+        quality - 1
+
+2. (AgedBrie || TAFKAL80ETC) && quality < 50
+    quality + 1
+    TAFKAL80ETC && sellIn < 11 && quality < 50
+        quality + 1
+    TAFKAL80ETC && sellIn < 6 && quality < 50
+        quality + 1
+
+3. !Sulfuras
+    sellIn - 1
+
+4. sellIn < 0
+    !Aged Brie && !TAFKAL80ETC
+        quality > 0 && !Sulfuras
+            quality - 1
+    !Aged Brie && TAFKAL80ETC
+        quality - quality
+    Aged Brie && quality < 50
+        quality + 1
+ */
